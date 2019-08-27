d2697eb5dada5f625913d5c3e05b9a42b1338f90
関谷侑希 <intern2@cp.synergy101.jp>
2019-08-27 14:52:46 +0900

    save data and deleteall by repository method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.repository.ShopRepository;
+import com.example.demo.domain.Shop;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(ShopRepository repository) {
+		return (args) -> {
+			// save a couple of shops
+			repository.deleteAll();
+			repository.save(new Shop("シナジーフレンチ", "100-500", "京都"));
+			repository.save(new Shop("シナジーイタリアン", "-100", "兵庫"));
+			repository.save(new Shop("シナジー中華", "1000-5000", "奈良"));
+			repository.save(new Shop("シナジー和食", "10000-", "和歌山"));
+			repository.save(new Shop("シナジーファミレス", "2000-5000", "大阪"));
+			
+		};
 	}
 
 }
--- src/main/java/com/example/demo/domain/Shop.java
+++ src/main/java/com/example/demo/domain/Shop.java
@@ -18,34 +18,22 @@ public class Shop {
     @NotEmpty
     private String place;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+    public Shop() {}
+
+    public Shop(String name, String priceRange, String place) {
         this.name = name;
-    }
-
-    public String getPriceRange() {
-        return priceRange;
-    }
-
-    public void setPriceRange(String priceRange) {
         this.priceRange = priceRange;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
         this.place = place;
     }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getPriceRange() { return priceRange; }
+    public void setPriceRange(String priceRange) { this.priceRange = priceRange; }
+    public String getPlace() { return place; }
+    public void setPlace(String place) { this.place = place; }
 
     @Override
     public String toString() {
